2073c1eb32cb656dc5c52719fee5ef25dcbb549b
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-07-07 23:56:12 +0300

    Update city.js

--- WeatherForecastApp/Scripts/city.js
+++ WeatherForecastApp/Scripts/city.js
@@ -1,13 +1,18 @@
 ﻿$(document).ready(function () {
-    $('input[type="checkbox"]').click(function () {
+    $('input[type="checkbox"]').on("click", function () {
         if ($(this).prop("checked") == true) {
             $("#editorId").prop("disabled", false);
+            $("#editorId").prop("required", true);
             $("#enumId").attr("disabled", "disabled");
         }
         else if ($(this).prop("checked") == false) {
             $("#editorId").prop("disabled", true);
             $("#editorId").val("");
+            $("#editorId").prop("required", false);
             $("#enumId").removeAttr("disabled", "disabled");
         }
     });
+});
+
+$("#buttonId").on("click", function () {
 });
