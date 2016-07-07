47b0b5a4fef62b1e8d3ca58b76def0f90e006ca9
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-07-07 15:55:38 +0300

    Add enum for cities.

---
+++ WeatherForecastApp/Models/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeatherForecastApp.Models
+{
+    enum EnumCity
+    {
+        Lviv,
+        Kyiv,
+        Kharkiv,
+        Dnipro,
+        Odessa
+    }
+}
