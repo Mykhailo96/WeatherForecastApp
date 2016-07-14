36c7b9300b2abcae7493a55cf6e0945d153e021b
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-07-14 22:38:02 +0300

    Add list for default cities.

---
+++ WeatherForecastApp/Controllers/CityController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherForecastApp.Models;
+
+namespace WeatherForecastApp.Controllers
+{
+    public class CityController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CityController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+
+        // GET: City
+        public ActionResult Index()
+        {
+            var cities = _context.CityByDefaults.ToList();
+
+            return View("List", cities);
+        }
+    }
+}
