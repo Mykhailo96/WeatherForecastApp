8838ab38cfaf185490ce07af6d4fc4006ae7206f
Mykhailo Voronovskii <Mykhailoblack@ukr.net>
2016-07-14 19:01:59 +0300

    Add connection string, db context and table for default cities.

---
+++ WeatherForecastApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WeatherForecastApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<CityByDefault> CityByDefaults { get; set; }
+
+        public ApplicationDbContext() : base("WeatherForecastApp")
+        {
+        }
+    }
+}
---
+++ WeatherForecastApp/Models/CityByDefault.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WeatherForecastApp.Models
+{
+    public class CityByDefault
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Name { get; set; }
+    }
+}
